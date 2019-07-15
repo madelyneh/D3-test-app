6d9964d637bba02d3b7562c7288e264c2db8a325
Madelyn Helton <madelyn.e.helton@gmail.com>
2019-07-15 15:56:31 -0500

    adding the new chart for the projections

--- src/app/components/seasonal-projection/seasonal-projection.component.ts
+++ src/app/components/seasonal-projection/seasonal-projection.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  Chart,
+  ChartAssist,
+  IChartSeries,
+  ILineAccessors,
+  LineAccessors,
+  LinearScale,
+  LineRenderer,
+  Scales,
+  TimeScale,
+  XYGrid
+} from '@solarwinds/nova-bits';
+import moment from 'moment/moment';
 
 @Component({
   selector: 'app-seasonal-projection',
@@ -9,7 +22,60 @@ export class SeasonalProjectionComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  public chart = new Chart(new XYGrid());
+
+  public chartAssist: ChartAssist = new ChartAssist(this.chart);
+
+  public ngOnInit() {
+      // providing chartAssist colors and markers to LineAccessors will share them with the line chart
+      const accessors = new LineAccessors(this.chartAssist.palette.standardColors, this.chartAssist.markers);
+      const renderer = new LineRenderer();
+      const scales: Scales = {
+          x: new TimeScale(),
+          y: new LinearScale(),
+      };
+
+      const seriesSet: IChartSeries<ILineAccessors>[] = getData().map(d => ({
+          ...d,
+          accessors,
+          renderer,
+          scales,
+      }));
+
+      // chart assist needs to be used to update data
+      this.chartAssist.update(seriesSet);
   }
 
+
 }
+
+/* Chart data */
+function getData() {
+    const format = 'YYYY-MM-DDTHH:mm:ssZ';
+
+    return [
+        {
+            id: 'series-1',
+            name: 'Series 1',
+            data: [
+                { x: moment('2016-12-25T15:14:29.909Z', format), y: 30 },
+                { x: moment('2016-12-27T15:14:29.909Z', format), y: 95 },
+                { x: moment('2016-12-29T15:14:29.909Z', format), y: 15 },
+                { x: moment('2016-12-31T15:14:29.909Z', format), y: 60 },
+                { x: moment('2017-01-03T15:14:29.909Z', format), y: 35 },
+            ],
+        },
+        {
+            id: 'series-2',
+            name: 'Series 2',
+            data: [
+                { x: moment('2016-12-25T15:14:29.909Z', format), y: 60 },
+                { x: moment('2016-12-27T15:14:29.909Z', format), y: 40 },
+                { x: moment('2016-12-29T15:14:29.909Z', format), y: 70 },
+                { x: moment('2016-12-31T15:14:29.909Z', format), y: 45 },
+                { x: moment('2017-01-03T15:14:29.909Z', format), y: 90 },
+            ],
+        },
+    ];
+}
+
