7ad87883b9495f62388ed59af38fa57a5963aa6a
Madelyn Helton <madelyn.e.helton@gmail.com>
2019-07-16 13:48:44 -0500

    added new layout and table component

---
+++ src/app/models/basicTable.ts
@@ -0,0 +1,10 @@
+export interface BasicTableModel {
+  position: number;
+  name: string;
+  features: any;
+  asset: string;
+  location: string;
+  status: string;
+  outages: number;
+  checks: any;
+}
